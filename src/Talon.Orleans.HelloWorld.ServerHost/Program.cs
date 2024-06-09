d507878b9c2c6b4459290300654144de17d49eb9
Andre DeCarvalho <decarvalhoa@gmail.com>
2024-06-09 16:55:36 -0700

    adding simple grain call as webapi

--- src/Talon.Orleans.HelloWorld.ServerHost/Program.cs
+++ src/Talon.Orleans.HelloWorld.ServerHost/Program.cs
@@ -2,9 +2,12 @@
 
 internal static class Program
 {
+  /// <remarks>Main entry point of the web server</remarks>
   static async Task Main(string[] args)
   {
     var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.ConfigureHelloWorldServerHostServices();
 
     builder.Host.UseOrleans((hostBuilder, siloBuilder) =>
     {
@@ -18,7 +21,17 @@ internal static class Program
     host
       .UseDefaultFiles()
       .UseStaticFiles();
-    
+    host.MapControllers();
+
     await host.RunAsync();
+  }
+
+  /// <summary>performs all necessary dependency injection.</summary>
+  private static IHostBuilder ConfigureHelloWorldServerHostServices(this IHostBuilder builder)
+  {
+    return builder.ConfigureServices(svcs =>
+    {
+      svcs.AddControllers();
+    });
   }
 }
