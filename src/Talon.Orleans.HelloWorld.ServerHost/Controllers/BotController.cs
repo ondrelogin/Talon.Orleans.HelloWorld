704c67a8fef8dec09e5307f43d8bfb08edb03c6b
Andre <ondrelogin@users.noreply.github.com>
2024-06-09 23:05:49 -0700

    Add webapi (#5)
    * adding simple grain call as webapi
    
    * add swagger / openapi

---
+++ src/Talon.Orleans.HelloWorld.ServerHost/Controllers/BotController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Talon.Orleans.HelloWorld.Common;
+
+namespace Talon.Orleans.HelloWorld.ServerHost.Controllers;
+
+/// <summary>
+/// Web API Controller for basic Orleans access.
+/// </summary>
+[Route("api/bot")]
+[ApiController]
+public class BotController : ControllerBase
+{
+  private readonly IClusterClient _grainFactory;
+
+  public BotController(IClusterClient grainFactory)
+  {
+    _grainFactory = grainFactory;
+  }
+
+  /// <summary>calls say hello</summary>
+  [Route("sayhello")]
+  [HttpGet]
+  public async Task<string> SayHelloAsync(string clientName)
+  {
+    var bot = _grainFactory.GetGrain<IBot>(clientName);
+    return await bot.SayHello(clientName);
+  }
+}
--- src/Talon.Orleans.HelloWorld.ServerHost/Program.cs
+++ src/Talon.Orleans.HelloWorld.ServerHost/Program.cs
@@ -2,9 +2,12 @@
 
 internal static class Program
 {
+  /// <remarks>Main entry point of the web server</remarks>
   static async Task Main(string[] args)
   {
     var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.ConfigureHelloWorldServerHostServices();
 
     builder.Host.UseOrleans((hostBuilder, siloBuilder) =>
     {
@@ -18,7 +21,26 @@ internal static class Program
     host
       .UseDefaultFiles()
       .UseStaticFiles();
-    
+    host.MapControllers();
+
+#if DEBUG
+    if (host.Environment.IsDevelopment())
+    {
+      host.UseSwagger();
+      host.UseSwaggerUI();
+    }
+#endif
+
     await host.RunAsync();
+  }
+
+  /// <summary>performs all necessary dependency injection.</summary>
+  private static IHostBuilder ConfigureHelloWorldServerHostServices(this IHostBuilder builder)
+  {
+    return builder.ConfigureServices(svcs =>
+    {
+      svcs.AddControllers();
+      svcs.AddSwaggerGen();
+    });
   }
 }
